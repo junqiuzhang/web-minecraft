0e8b08ffa22543aba5d6f14032cd80c169da1879
zhangjunqiu <zhangjunqiu@beisen.com>
2019-12-09 18:51:47 +0800

    feat(ground)缓冲

--- src/interact/bind-hover.ts
+++ src/interact/bind-hover.ts
@@ -17,7 +17,7 @@ function handleMouseMove({
   return throttle(function (this: HTMLElement, event: MouseEvent) {
     const mousePosition = new THREE.Vector2((event.clientX / window.innerWidth) * 2 - 1, - (event.clientY / window.innerHeight) * 2 + 1);
     raycaster.setFromCamera(mousePosition, camera);
-    intersects = raycaster.intersectObjects(scene.children);
+    intersects = raycaster.intersectObjects(scene.children, true);
     if (intersects.length > 0) {
       engine.onHover({
         intersect: intersects[0]
--- src/setting/ground.ts
+++ src/setting/ground.ts
@@ -4,7 +4,7 @@ function ground({
   scene,
   camera
 }: ICommonParam) {
-  const mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(2000, 2000), new THREE.MeshPhongMaterial({ color: 0xffffff, depthWrite: false }));
+  const mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(2000, 2000), new THREE.MeshPhongMaterial({ color: 0xffffff }));
   mesh.rotation.x = - Math.PI / 2;
   scene.add(mesh);
   const grid = new THREE.GridHelper(2000, 2000);
