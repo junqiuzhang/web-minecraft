87617b69e07468c5c5ed6268dea0dad03c7247f5
zhangjunqiu <zhangjunqiu@beisen.com>
2019-12-12 19:28:55 +0800

    refactor(engine)remove cannon

--- src/engine/index.ts
+++ src/engine/index.ts
@@ -1,5 +1,4 @@
 import * as THREE from 'three';
-import * as CANNON from 'cannon';
 import Cube from '../geometry/cube';
 import { StepLength, CrashDistance } from '../constant';
 type Direction = 'up' | 'down' | 'left' | 'right';
@@ -16,8 +15,6 @@ class engine {
   private camera: THREE.Camera;
   private renderer: THREE.Renderer;
   private state: IState;
-  private cannon: CANNON.World;
-  private threeBindCannon: Array<{ three: THREE.Mesh, cannon: CANNON.Body }>
   private overMesh: THREE.Mesh;
   private cameraMesh: THREE.Mesh;
   mesh: THREE.Mesh;
@@ -30,45 +27,18 @@ class engine {
     this.scene = scene;
     this.camera = camera;
     this.renderer = renderer;
-    this.threeBindCannon = [];
     this.state = {
       isShiftDown: false
     }
-    this.update = this.update.bind(this);
-    this.mountCannon();
     this.mountOverMesh();
     this.mountCameraMesh();
-    this.update();
-  }
-  update() {
-    requestAnimationFrame(this.update);
-    this.cannon.step(1 / 60);
-    for (let i = 0; i < this.threeBindCannon.length; i++) {
-      const obj = this.threeBindCannon[i];
-      const { position, quaternion } = obj.cannon;
-      obj.three.position.copy(new THREE.Vector3(position.x, position.y, position.z));
-      obj.three.quaternion.copy(new THREE.Quaternion(quaternion.x, quaternion.y, quaternion.z, quaternion.w));
-    }
-  }
-  private mountCannon() {
-    this.cannon = new CANNON.World();
-    this.cannon.gravity.set(0, -10, 0);
-    this.cannon.broadphase = new CANNON.NaiveBroadphase();
-    
-    const groundBody = new CANNON.Body({ 
-      mass: 0,
-      position: new CANNON.Vec3(0, -0.5, 0),
-      shape: new CANNON.Plane()
-    });
-    groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), - Math.PI / 2);    
-    this.addCannon(groundBody);
   }
   private mountOverMesh() {
     const rollOverGeo = new THREE.BoxBufferGeometry(1, 1, 1);
     const rollOverMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, opacity: 0.5, transparent: true });
     const rollOverMesh = new THREE.Mesh(rollOverGeo, rollOverMaterial);
     this.overMesh = rollOverMesh;
-    this.add(rollOverMesh);
+    this.addMesh(rollOverMesh);
   }
   private mountCameraMesh() {
     const geometry = new THREE.BoxGeometry(1, 2, 1);
@@ -84,38 +54,14 @@ class engine {
   add(target: THREE.Object3D) {
     this.scene.add(target);
   }
-  addCannon(object: CANNON.Body) {
-    this.cannon.addBody(object);
-  }
   remove(target: THREE.Object3D) {
     this.scene.remove(target);
   }
-  removeCannon(object: CANNON.Body) {
-    this.cannon.remove(object);
-  }
   addMesh(target: THREE.Mesh) {
-    target.geometry.computeBoundingBox();
-    const { x, y, z } = target.position;
-    const diffX = target.geometry.boundingBox.max.x - target.geometry.boundingBox.min.x;
-    const diffY = target.geometry.boundingBox.max.y - target.geometry.boundingBox.min.y;
-    const diffZ = target.geometry.boundingBox.max.z - target.geometry.boundingBox.min.z;
-    const object = new CANNON.Body({
-      mass: diffX * diffY * diffZ, // kg
-      position: new CANNON.Vec3(x, y, z), // m
-      shape: new CANNON.Box(new CANNON.Vec3(diffX, diffY, diffZ)) // m
-    })
     this.add(target);
-    this.addCannon(object);
-    this.threeBindCannon.push({
-      three: target,
-      cannon: object
-    })
   }
   removeMesh(target: THREE.Mesh) {
-    const bindObject = this.threeBindCannon.find(obj => obj.three === target);
-    this.remove(bindObject.three);
-    this.removeCannon(bindObject.cannon);
-    this.threeBindCannon = this.threeBindCannon.filter(obj => obj.three !== target);
+    this.remove(target);
   }
   onClick(intersects: THREE.Intersection[]) {
     const { isShiftDown } = this.state;
