af3e2a5f9116e5a07d07f24d990bf16593e07661
zhangjunqiu <zhangjunqiu@beisen.com>
2020-08-20 15:15:02 +0800

    fix(cube)name

--- src/geometry/cube.ts
+++ src/geometry/cube.ts
@@ -4,7 +4,6 @@ class Cube extends THREE.Mesh {
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshLambertMaterial({ color: 0x00ff00, ...param });
     super(geometry, material);
-    this.name = 'cube';
   }
 }
 export default Cube;
